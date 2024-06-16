96dbfe1b4341f5c9787e21efd7cbfbccb647d1af
ronan-alt <ridel.ronan@gmail.com>
2024-06-16 18:09:18 +0200

    Delete content directory

--- content/alderin-tapisserie/scripts_tapisserie/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.barre');
-    menuToggle.addEventListener('click', function () {
-        navLinks.classList.toggle('active');
-    });
-});
